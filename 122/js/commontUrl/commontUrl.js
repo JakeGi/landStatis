aa08df082c7bb2330917d1a39f857e1301b6bddd
junjie jiang <junjiejiang@junjiedeMacBook-Air.local>
2018-02-22 15:52:45 +0800

    。。。

--- 122/js/commontUrl/commontUrl.js
+++ 122/js/commontUrl/commontUrl.js
@@ -11,3 +11,4 @@ var commentBaseUrl = "http://hit.huidatech.cn";
 var listUrl = "/hdPrecisionAgri/action/work?";
 var userMsg = "/hdPrecisionAgri/action/dept?";
 var showUserMesg ="/hdPrecisionAgri/action/work?";
+var listDetail = "/hdPrecisionAgri/action/workplot?";
