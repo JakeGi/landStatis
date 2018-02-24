4efa53a6f1a9ff07f97bd5156685a60dfd359411
jyyl <jyyl@jyyldeMac-mini.local>
2018-02-24 18:02:48 +0800

    11

--- 122/js/commontUrl/commontUrl.js
+++ 122/js/commontUrl/commontUrl.js
@@ -12,3 +12,4 @@ var listUrl = "/hdPrecisionAgri/action/work?";
 var userMsg = "/hdPrecisionAgri/action/dept?";
 var showUserMesg ="/hdPrecisionAgri/action/work?";
 var listDetail = "/hdPrecisionAgri/action/workplot?";
+var getMapPhoto = "http://101.201.155.153:8080/hpaCloud/action/trace?";
